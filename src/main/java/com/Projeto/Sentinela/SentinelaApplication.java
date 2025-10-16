ea67bd06c9708dd25fb99a2127fad60693f1f9d2
mattiias00 <matheusmatts44@gmail.com>
2025-10-15 22:17:30 -0300

    Adicionando dependencias de banco de dados e de springboot

---
+++ src/main/java/com/Projeto/Sentinela/SentinelaApplication.java
@@ -0,0 +1,13 @@
+package com.Projeto.Sentinela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelaApplication.class, args);
+	}
+
+}
